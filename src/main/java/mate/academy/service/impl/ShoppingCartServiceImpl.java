6bca7ec34e458d39bad40ec3006c941ae0d8cc02
PatrykWski <patrykwiszniewski96@gmail.com>
2026-05-31 14:57:04 +0200

    Shopping cart service impl has been completly finished. All methods has been implemented

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,22 +1,30 @@
 package mate.academy.service.impl;
 
+import java.util.ArrayList;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
+import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.MovieSession;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.Ticket;
 import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.ShoppingCartService;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Inject
-    TicketDao ticketDao;
+    private TicketDao ticketDao;
     @Inject
-    ShoppingCartDao shoppingCartDao;
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private AuthenticationService authenticationService;
 
     @Override
     public void addSession(MovieSession movieSession, User user) {
@@ -34,17 +42,41 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public ShoppingCart getByUser(User user) {
-        return shoppingCartDao.getByUser(user).orElseThrow
-                (() -> new RuntimeException("This shopping cart doesn't exist"));
+        return shoppingCartDao.getByUser(user).orElseThrow(
+                () -> new RuntimeException("This shopping cart doesn't exist"));
     }
 
     @Override
     public void registerNewShoppingCart(User user) {
-
+        if (user != null) {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.setUser(user);
+            shoppingCart.setTickets(new ArrayList<>());
+            shoppingCartDao.add(shoppingCart);
+        } else {
+            throw new RuntimeException("This user doesn't exist");
+        }
     }
 
     @Override
     public void clear(ShoppingCart shoppingCart) {
-
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            shoppingCart.getTickets().clear();
+            session.merge(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't clear shopping cart", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
